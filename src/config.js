757cb4eea2d606b011860657b0d0be35fbdc419a
Puyodead1 <23562356riley@gmail.com>
2020-07-15 00:08:38 -0400

    update bot endpoint url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  // bot_url: "https://status.ittim.xyz/api/stats",
-  bot_url: "http://127.0.0.1",
+  bot_url: "https://status.ittim.xyz/api/stats",
+  //bot_url: "http://127.0.0.1",
   endpoints: {
     stats: "/api/stats",
     leaderboards: "/api/leaderboard",
