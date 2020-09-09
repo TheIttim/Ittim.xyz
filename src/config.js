ab7b12f689a663c8d657982c449044b097fd4608
Puyodead1 <23562356riley@gmail.com>
2020-09-08 20:04:04 -0400

    config [ Yep, Jordan was right on this one. ]

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  API_URL: "http://127.0.0.1",
-  //API_URL: "https://api.moguldevs.xyz",
+  //API_URL: "http://127.0.0.1",
+  API_URL: "https://api.moguldevs.xyz",
   //bot_url: "http://127.0.0.1",
   ENDPOINTS: {
     STATS: "/api/stats",
@@ -10,7 +10,7 @@ export default {
   },
   OWNERS: ["183790855813988353", "213247101314924545", "311553339261321216"],
   OAUTH: {
-    REDIRECT_URI: "http://127.0.0.1:3000/login/callback",
+    REDIRECT_URI: "https://moguldevs.xyz/login/callback",
     SCOPE: "identify",
     CLIENT_ID: "519954424270028811",
   },
